88ee11583ea722644676dc05623380747cfbbda7
Sanji <121347953+sanjiie@users.noreply.github.com>
2024-10-01 15:18:16 +0700

    Add files via upload

---
+++ src/main/java/com/example/mylivedata/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.mylivedata
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
+import com.example.mylivedata.databinding.ActivityMainBinding
+import androidx.lifecycle.Observer
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var liveDataTimerViewModel: MainViewModel
+    private lateinit var activityMainBinding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+
+        liveDataTimerViewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        subscribe()
+    }
+
+    private fun subscribe() {
+        val elapsedTimeObserver = Observer<Long?> { aLong ->
+            val newText = this@MainActivity.resources.getString(R.string.seconds, aLong)
+            activityMainBinding.timerTextview.text = newText
+        }
+        liveDataTimerViewModel.getElapsedTime().observe(this, elapsedTimeObserver)
+    }
+}
